5c68c181a70b7290f9648649194997ed59317c2f
Sahar <sahar.abbasi13@gmail.com>
2025-03-06 04:06:21 +0330

    vowel and consonant sounds.py

--- vowel and consonant sounds.py
+++ vowel and consonant sounds.py
@@ -1,5 +1,6 @@
+mylist=['a','e','i','o','u']
 character= input('enter a character:')
-if character in ('a','e','i','o','u'):
+if character is ('a','e','i','o','u'):
     print('your character is vowel sound')
 else:
-    print ('your character is consonant')
+    print ('your character is consonant, try an other character')
